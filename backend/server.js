64828aad70f86167955e7e7ce252fc809f9fefbe
AjayGhimire <ajayghimire1998@gmail.com>
2022-12-06 16:46:08 +1100

    fkniiiiiiepororor

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 const path = require("path");
 
